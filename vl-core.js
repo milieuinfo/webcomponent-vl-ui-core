08ef6418e09a0e8fdfc77e347dccc9a444d4b257
Tom Coemans <tom.coemans@gmail.com>
2019-03-15 14:45:37 +0100

    WEBC-16 vl-core

---
+++ vl-core.js
@@ -0,0 +1,1 @@
+(()=>{function t(){var t=document.createElement("link");return t.setAttribute("id",e),t.setAttribute("rel","stylesheet"),t.setAttribute("type","text/css"),t.setAttribute("href","../style.css"),t}const e="vl-core-style";!function(){if(!document.head.querySelector("#"+e)){var n=t();document.head.appendChild(n)}}()})();
---
+++ vl-core.src.js
@@ -0,0 +1,20 @@
+(() => {
+    const id = 'vl-core-style';
+    addStyle();
+
+    function addStyle() {
+        if (!document.head.querySelector('#' + id)) {
+            var style = getStyle();
+            document.head.appendChild(style);
+        }
+    }
+
+    function getStyle() {
+        var link = document.createElement('link');
+        link.setAttribute('id', id);
+        link.setAttribute('rel', 'stylesheet');
+        link.setAttribute('type', 'text/css');
+        link.setAttribute('href', '../style.css');
+        return link;
+    }
+})();
