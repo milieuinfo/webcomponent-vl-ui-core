9e3ee0b4f06697d560bc0fb3a50c819a26652df3
Tom Coemans <tom.coemans@gmail.com>
2019-03-22 10:10:15 +0100

    WEBC-16 root webcomponent element introductie

--- vl-core.src.js
+++ vl-core.src.js
@@ -18,3 +18,51 @@
         return link;
     }
 })();
+
+/**
+ * `vl-element`
+ * De root element class.
+ */
+export class VlElement extends HTMLElement {
+    constructor(html) {
+        super();
+        if (html) {
+            this.__shadow(html);
+        }
+    }
+
+    static get observedAttributes() {
+        return this._observedAttributes;
+    }
+
+    attributeChangedCallback(attr, oldValue, newValue) {
+        const callback = this['_' + attr + 'ChangedCallback'];
+        if (callback) {
+            callback.call(this, oldValue, newValue);
+        } else {
+            console.info('_' + attr + 'ChangedCallback is not defined');
+        }
+    }
+
+    get _element() {
+        return this._shadow.lastElementChild;
+    }
+
+    _template(html) {
+        const template = document.createElement('template');
+        template.innerHTML = html;
+        return template.content;
+    }
+
+    _changeClass(element, oldValue, newValue) {
+        if (element.classList.contains(oldValue)) {
+            element.classList.remove(oldValue);
+        }
+        element.classList.add(newValue);
+    }
+
+    __shadow(html) {
+        this._shadow = this.attachShadow({mode: 'open'});
+        this._shadow.innerHTML = html;
+    }
+}
