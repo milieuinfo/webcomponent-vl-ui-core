6e26cfa9f9113cdbc233ecd4abfb09a0f50fec8e
Tom Coemans <tom.coemans@gmail.com>
2019-04-09 13:33:57 +0200

    WEBC-60 toggle class functie toegevoegd en change attribute functie private gemaakt

--- vl-core.src.js
+++ vl-core.src.js
@@ -20,10 +20,16 @@
 })();
 
 /**
- * `vl-element`
- * De root element class.
+ * `VlElement`
+ * De root element class voor custom HTML elementen.
  */
 export const VlElement = (SuperClass) => class extends SuperClass {
+    /**
+     * VlElement constructor die een shadow DOM voorziet op basis van de HTML {Literal} parameter.
+     * 
+     * @param {Literal} html - HTML literal die de DOM representeert
+     * @Return {void}
+     */
     constructor(html) {
         super();
         if (html) {
@@ -35,14 +41,29 @@ export const VlElement = (SuperClass) => class extends SuperClass {
         return this._observedAttributes.concat(this._observedClassAttributes).concat(this._observedChildClassAttributes);
     }
 
+    /**
+     * Een lijst van attributen die geobserveerd zullen worden. Bij een wijziging van de attribuut waarde zal een functie (`_attribuutChangedCallback`) functie aangeroepen worden met de oude en nieuwe waarde als parameters. Indien de attribuut `-` bevat in de naam zal dit vervangen worden door `_` bij het aanroepen van de functie (`_attribuut_naamChangedCallback`).
+     * 
+     * @Return {Array} array van attributen waar naar geluisterd moet worden
+     */
     static get _observedAttributes() {
         return [];
     }
 
+    /**
+     * Een lijst van attributen die geobserveerd zullen worden. Bij een wijziging van de attribuut waarde zal een class toegevoegd worden aan het root element. De naam van de class is gebaseerd op de element prefix (`_classPrefix`) en de naam van het attribuut.
+     * 
+     * @Return {Array} array van attributen waar naar geluisterd moet worden
+     */
     static get _observedClassAttributes() {
         return [];
     }
 
+    /**
+     * Een lijst van attributen die geobserveerd zullen worden. Bij een wijziging van de attribuut waarde zal een class toegevoegd worden aan het eerste shadow DOM element. De naam van de class is gebaseerd op de element prefix (`_classPrefix`) en de naam van het attribuut.
+     * 
+     * @Return {Array} array van attributen waar naar geluisterd moet worden
+     */
     static get _observedChildClassAttributes() {
         return [];
     }
@@ -72,28 +93,82 @@ export const VlElement = (SuperClass) => class extends SuperClass {
         }
     }
 
+    /**
+     * De class prefix bepaalt de prefix van het class attribuut dat automatisch toegevoegd wordt op basis van attributen.
+     * 
+     * @Return {void}
+     */
     get _classPrefix() {
         console.error('class prefix is undefined');
     }
 
+    /**
+     * DOM element getter.
+     * 
+     * @Return {Element}
+     */
     get _element() {
         return this._shadow.lastElementChild;
     }
 
+    /**
+     * Genereert een {HTMLTemplateElement} template met de HTML Literal.
+     * 
+     * @param {Literal} html - HTML literal
+     * @Return {HTMLTemplateElement}
+     */
     _template(html) {
         const template = document.createElement('template');
         template.innerHTML = html;
         return template.content;
     }
 
+    /**
+     * Het class attribuut op basis van de oude waarde zal vervangen worden door het class attribuut op basis van de nieuwe waarde.
+     * 
+     * @param {Element} element - HTML element
+     * @param {Object} oldValue - oude waarde
+     * @param {Object} newValue - nieuwe waarde
+     * @param {String} classPrefix - class prefix 
+     * @return {void}
+     */
     _changeClass(element, oldValue, newValue, classPrefix) {
         if (element.classList.contains((classPrefix || this._classPrefix) + oldValue)) {
             element.classList.remove((classPrefix || this._classPrefix) + oldValue);
         }
-        element.classList.add((classPrefix || this._classPrefix) + newValue);
+
+        if (newValue != undefined) {
+            element.classList.add((classPrefix || this._classPrefix) + newValue);
+        }
     }
 
-    _changeAttribute(element, oldValue, newValue, attribute, classPrefix) {
+    /**
+     * Zal op basis van de attribuut waarde de class verwijderen of toevoegen.
+     * 
+     * @param {Element} element - HTML element
+     * @param {Object} value - attribuut waarde
+     * @param {String} clazz- class waarde 
+     * @return {void}
+     */
+    _toggleClass(element, value, clazz) {
+        if (value != undefined && !!value) {
+            element.classList.add(clazz);
+        } else {
+            element.classList.remove(clazz);
+        }
+    }
+
+    /**
+     * Wijzigt het class attribuut van het HTML element als de oude en nieuwe waarde van het attribuut niet identiek zijn. Indien de nieuwe waarde bestaat zal een class toegevoegd worden op basis van deze waarde, anders zal de bestaande class op basis van de oude waarde verwijderd worden.
+     * 
+     * @param {Element} element - HTML element
+     * @param {Object} oldValue - oude waarde 
+     * @param {Object} newValue - nieuwe waarde 
+     * @param {String} attribute - attribuut naam
+     * @param {String} classPrefix - class prefix
+     * @return {void}
+     */
+    __changeAttribute(element, oldValue, newValue, attribute, classPrefix) {
         if (oldValue != newValue) {
             if (this.getAttribute(attribute) != undefined) {
                 element.classList.add((classPrefix || this._classPrefix) + attribute);
@@ -103,18 +178,36 @@ export const VlElement = (SuperClass) => class extends SuperClass {
         }
     }
 
+    _bevatClass(value) {
+        return this._element.classList.contains(value);
+    }
+
     __shadow(html) {
         this._shadow = this.attachShadow({mode: 'open'});
         this._shadow.innerHTML = html;
     }
 }
 
+/**
+ * `NativeVlElement`
+ * De root element class voor native HTML elementen.
+ */
 export const NativeVlElement = (SuperClass) => class extends VlElement(SuperClass) {
+    /**
+     * NativeVlElement constructor die het element CSS link element automatisch zal toevoegen aan de DOM head tag.
+     * 
+     * @Return {void}
+     */
     constructor() {
         super();
         this.__generateStyleLink();
     }  
     
+    /**
+     * DOM element getter.
+     * 
+     * @Return {Element}
+     */
     get _element() {
         return this;
     }
