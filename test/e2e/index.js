5653a4e22d05c43b2c00973cf52196776e24abe4
cambiph <incubusattax@gmail.com>
2019-11-19 14:10:03 +0100

    Expose config

--- test/e2e/index.js
+++ test/e2e/index.js
@@ -1,4 +1,5 @@
 const { assert, driver } = require('./test.js');
 const { Page } = require('./pages/page.js');
 const { VlElement } = require('./components/vl-element.js');
-module.exports = { assert, driver, Page, VlElement };
+const { config } = require('./config.js')
+module.exports = { assert, driver, Page, VlElement, config };
