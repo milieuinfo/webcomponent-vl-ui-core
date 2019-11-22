7fbe886a8f263a74932773c63617b0ea72a6fd00
cambiph <incubusattax@gmail.com>
2019-11-22 10:45:26 +0100

    Export alles via index.js

---
+++ index.js
@@ -0,0 +1,7 @@
+const VlElement = require('./test/e2e/components/vl-element');
+const Page = require('./test/e2e/pages/page');
+
+const Config = require('./test/e2e/config');
+const Test = require('./test/e2e/test');
+
+module.exports = { VlElement, Page, Config, Test }
