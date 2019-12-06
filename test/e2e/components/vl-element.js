ed86c178616c75ede65e2dedadc2b9f9486ae1e7
Tom Coemans <tom.coemans@gmail.com>
2019-12-06 14:57:54 +0100

    introductie e2e functie om de focus te controleren en constructor ondersteuning voor web elementen

--- test/e2e/components/vl-element.js
+++ test/e2e/components/vl-element.js
@@ -2,14 +2,20 @@ const { WebElement } = require('selenium-webdriver');
 const { By } = require('selenium-webdriver');
 
 class VlElement extends WebElement {
-    constructor(driver, selector) {
+    constructor(driver, identifier) {
         return (async () => {
-            super(driver, await driver.findElement(By.css(selector)).getId());
+            typeof identifier === 'string' ? super(driver, await driver.findElement(By.css(identifier)).getId()) : super(driver, await identifier.getId());
             this.driver = driver;
-            this.selector = selector;
-            this.shadowRoot = await this.driver.executeScript('return arguments[0].shadowRoot.lastElementChild', this);
+            if (await this.driver.executeScript('return arguments[0].shadowRoot != undefined', this)) {
+                this.shadowRoot = await new VlElement(this.driver, await this.driver.executeScript('return arguments[0].shadowRoot.lastElementChild', this));
+            }
             return this;
         })();
+    }
+
+    async findElement(selector) {
+        const element = await super.findElement(selector);
+        return await new VlElement(this.driver, element);
     }
 
     async getClassList() {
@@ -30,6 +36,19 @@ class VlElement extends WebElement {
 
     async hasText() {
         return (await this.getText()) != '';
+    }
+
+    async hasFocus() {
+        const getActiveElement = async (element) => {
+            if (element.shadowRoot) {
+                const activeElement = await (this.driver.executeScript('return arguments[0].shadowRoot.activeElement', element));
+                return getActiveElement(await new VlElement(this.driver, activeElement));
+            } else {
+                return element;
+            }
+        }
+        const activeElement = await getActiveElement(await new VlElement(this.driver, await this.driver.switchTo().activeElement()));
+        return WebElement.equals(this, activeElement);
     }
 }
 
