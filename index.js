b797d5e8b15756328c44b51cb660451fc7c7ef8a
Tom Coemans <tom.coemans@gmail.com>
2020-01-23 17:04:57 +0100

    export

--- index.js
+++ index.js
@@ -1,11 +1,9 @@
-const { NativeVlElement, VlElement, define, awaitScript, awaitUntil } = require('./vl-core.src.js');
-
 module.exports = { 
-    NativeVlElement, 
-    VlElement, 
-    define, 
-    awaitScript, 
-    awaitUntil,
+    NativeVlElement: require('./vl-core.src.js').NativeVlElement,
+    VlElement: require('./vl-core.src.js').VlElement,
+    define: require('./vl-core.src.js').define, 
+    awaitScript: require('./vl-core.src.js').awaitScript, 
+    awaitUntil: require('./vl-core.src.js').awaitUntil,
     Test: {
         VlElement: require('./test/e2e/components/vl-element'), 
         Page: require('./test/e2e/pages/page'), 
