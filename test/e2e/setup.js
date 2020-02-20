238d01eec46ae130bd69ca20fb631575cb0db4a1
cambiph <philippe.cambien@gmail.com>
2020-02-20 14:22:58 +0100

    Houd bij welke browsers er allemaal worden geopend zodat deze allemaal kunnen geclosed worden nadat alle testen klaar zijn

--- test/e2e/setup.js
+++ test/e2e/setup.js
@@ -7,14 +7,23 @@ chai.use(chaiAsPromised);
 var assert = chai.assert;
 
 process.env['webdriver.gecko.driver'] = "../../node_modules/geckodriver/geckodriver";
-process.env['webdriver.chrome.driver'] = "../../node_modules/chromedriver/lib/chromedriver"
+process.env['webdriver.chrome.driver'] = "../../node_modules/chromedriver/lib/chromedriver";
 
 let driver;
+let drivers = [];
 
 if (config.gridEnabled) {
     driver = new Builder().usingServer(config.gridUrl).forBrowser(config.browserName).build();
+    drivers.push(driver);
 } else {
     driver = new Builder().forBrowser(config.browserName).build(); 
+    drivers.push(driver);
 }
+
+after(async () => {
+    return drivers.forEach(async (driver) => {
+        return driver.quit();
+    });
+});
 
 module.exports = { assert, driver, By };
