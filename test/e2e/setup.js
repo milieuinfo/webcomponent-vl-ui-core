0d98206a1736f928e63d43b88f430bf51d53ffd5
coemans <tom.coemans@gmail.com>
2020-02-24 09:35:15 +0100

    E2E test verbeteringen (#54)

--- test/e2e/setup.js
+++ test/e2e/setup.js
@@ -7,14 +7,18 @@ chai.use(chaiAsPromised);
 var assert = chai.assert;
 
 process.env['webdriver.gecko.driver'] = "../../node_modules/geckodriver/geckodriver";
-process.env['webdriver.chrome.driver'] = "../../node_modules/chromedriver/lib/chromedriver"
+process.env['webdriver.chrome.driver'] = "../../node_modules/chromedriver/lib/chromedriver";
 
 let driver;
 
 if (config.gridEnabled) {
     driver = new Builder().usingServer(config.gridUrl).forBrowser(config.browserName).build();
 } else {
-    driver = new Builder().forBrowser(config.browserName).build(); 
+    driver = new Builder().forBrowser(config.browserName).build();
 }
+
+after(async () => {
+    return driver.quit();
+});
 
 module.exports = { assert, driver, By };
