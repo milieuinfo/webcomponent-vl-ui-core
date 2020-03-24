402960f16e38d366c2afd2f5982286d54afff301
Tom Coemans <tom.coemans@gmail.com>
2020-03-24 12:14:14 +0100

    added prefixed changed callback function support

--- src/vl-core.js
+++ src/vl-core.js
@@ -95,7 +95,7 @@ export const VlElement = (SuperClass) => {
                 this.__changeAttribute(this._element, oldValue, newValue, attribute);
             });
 
-            const callback = this['_' + attr.split('-').join('_') + 'ChangedCallback'];
+            const callback = this['_' + attr.split('-').join('_') + 'ChangedCallback'] || this['_' + `${VlElement.attributePrefix}${attr}`.split('-').join('_') + 'ChangedCallback'];
             if (callback) {
                 callback.call(this, oldValue, newValue);
             }
--- test/unit/vl-native-element.js
+++ test/unit/vl-native-element.js
@@ -16,4 +16,21 @@ class VlNativeElement extends NativeVlElement(HTMLSpanElement) {
     _attributeChangedCallback(oldValue, newValue) { }
 }
 
+class VlNativePrefixedElement extends NativeVlElement(HTMLSpanElement) {
+    static get _observedAttributes() {
+        return ['attribute'];
+    }
+
+    static get _observedClassAttributes() {
+        return ['class-attribute'];
+    }
+
+    get _classPrefix() {
+        return 'vl-span--';
+    }
+
+    _data_vl_attributeChangedCallback(oldValue, newValue) { }
+}
+
 define('vl-native-element', VlNativeElement, { extends: 'span' });
+define('vl-native-prefixed-element', VlNativePrefixedElement, { extends: 'span' });
