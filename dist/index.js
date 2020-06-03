b872b7284acd88caf69b89f1fd0f3c22ab7cf2de
Tom Coemans <tom.coemans@gmail.com>
2020-06-03 12:11:31 +0200

    cache remove

--- dist/index.js
+++
@@ -1,7 +0,0 @@
-const {vlElement, nativeVlElement, define, awaitScript, awaitUntil} = require('../dist/vl-core.src.js');
-const VlElement = vlElement;
-const NativeVlElement = nativeVlElement;
-
-module.exports = {
-  vlElement, VlElement, nativeVlElement, NativeVlElement, define, awaitScript, awaitUntil,
-};
