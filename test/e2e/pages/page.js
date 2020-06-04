0ba18d7e1bf3d723e380ea1f110d61752b7390a8
Tom Coemans <tom.coemans@gmail.com>
2020-06-04 10:12:14 +0200

    remove header and footer before e2e starts

--- test/e2e/pages/page.js
+++ test/e2e/pages/page.js
@@ -5,6 +5,10 @@ class Page {
 
   async load(url) {
     await this.driver.get(url);
+    const header = await new VlHeader(driver);
+    const footer = await new VlFooter(driver);
+    await header.remove();
+    await footer.remove();
   }
 }
 
