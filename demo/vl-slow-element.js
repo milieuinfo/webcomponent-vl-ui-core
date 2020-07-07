e7aa87159cb4501d905a00128d686856045eb7a3
Tom Coemans <tom.coemans@gmail.com>
2020-07-07 16:57:29 +0200

    removed class prefix

--- demo/vl-slow-element.js
+++ demo/vl-slow-element.js
@@ -3,12 +3,8 @@ import {vlElement, define} from '/src/vl-core.js';
 class VlSlowElement extends vlElement(HTMLElement) {
   constructor() {
     super(`
-              <div>Only visible when defined!</div>
-          `);
-  }
-
-  get _classPrefix() {
-    return 'vl-span--';
+      <div>Only visible when defined!</div>
+    `);
   }
 }
 
