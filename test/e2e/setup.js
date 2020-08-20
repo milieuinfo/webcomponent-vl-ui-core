014b66636352e98e627f634bdcfca818fd172a0a
cambiph <philippe.cambien@gmail.com>
2020-08-20 10:48:21 +0200

    Voeg Chrome options toe

--- test/e2e/setup.js
+++ test/e2e/setup.js
@@ -1,5 +1,6 @@
 const {Builder, By, Key} = require('selenium-webdriver');
 const config = require('./config');
+let chrome = require('selenium-webdriver/chrome');
 
 const chai = require('chai');
 const chaiAsPromised = require('chai-as-promised');
@@ -10,10 +11,18 @@ process.env['webdriver.gecko.driver'] = '../../node_modules/geckodriver/geckodri
 process.env['webdriver.chrome.driver'] = '../../node_modules/chromedriver/lib/chromedriver';
 
 let driver;
+let options = new chrome.Options();
+options.addArguments(['start-maximized', 'disable-infobars', '--disable-extensions', '--no-sandbox', '--disable-dev-shm-usage']);
 
 if (config.gridEnabled) {
+  if(config.browserName == 'chrome') {
+    driver = new Builder().usingServer(config.gridUrl).forBrowser('chrome').setChromeOptions(options).build();
+  }
   driver = new Builder().usingServer(config.gridUrl).forBrowser(config.browserName).build();
 } else {
+  if(config.browserName == 'chrome') {
+    driver = new Builder().usingServer(config.gridUrl).forBrowser('chrome').setChromeOptions(options).build();
+  }
   driver = new Builder().forBrowser(config.browserName).build();
 }
 
