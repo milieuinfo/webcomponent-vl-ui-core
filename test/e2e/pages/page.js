730c9de0ff385e49996eff1d24ae197cf98e36ab
Tom Coemans <tom.coemans@gmail.com>
2020-09-14 10:07:07 +0200

    wait for visible body element

--- test/e2e/pages/page.js
+++ test/e2e/pages/page.js
@@ -6,6 +6,8 @@ class Page {
   async load(url) {
     await this.driver.get(`${url}?no-header=true&no-footer=true`);
     await this.driver.manage().window().maximize();
+    const body = await this.driver.findElement(By.css('body'));
+    await this.driver.wait(until.elementIsVisible(body), 5000);
   }
 }
 
