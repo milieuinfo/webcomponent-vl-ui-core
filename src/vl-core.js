436041de581549bc5cd3d3f814c3c1d377d71f36
coemans <tom.coemans@gmail.com>
2020-10-07 14:47:56 +0200

    Deprecated polyfill vervangen (#100)

--- src/vl-core.js
+++ src/vl-core.js
@@ -1,4 +1,4 @@
-import '/node_modules/document-register-element/build/document-register-element.js';
+import '/node_modules/@ungap/custom-elements/min.js';
 
 export const vlElement = (SuperClass) => {
   /**
