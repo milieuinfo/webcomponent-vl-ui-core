9202aff4b985f143cffdba04ec248c6e0bc80304
Tom Coemans <tom.coemans@gmail.com>
2020-11-13 08:40:10 +0100

    fix e2e test

--- test/e2e/vl-core.test.js
+++ test/e2e/vl-core.test.js
@@ -2,14 +2,16 @@ const {assert, getDriver} = require('vl-ui-core').Test.Setup;
 const VlCorePage = require('./pages/vl-core.page');
 
 describe('vl-core', async () => {
+  let driver;
   let vlCorePage;
 
   before(() => {
-    vlCorePage = new VlCorePage(getDriver());
+    driver = getDriver();
+    vlCorePage = new VlCorePage(driver);
     return vlCorePage.load();
   });
 
-  it('Een element wordt pas visible als het defined wordt', async () => {
+  it('een element wordt pas visible als het defined wordt', async () => {
     const slowElement = await vlCorePage.getSlowElement();
     await assert.eventually.isFalse(slowElement.isDisplayed());
     await driver.wait(async () => await slowElement.isDisplayed(), 3000);
