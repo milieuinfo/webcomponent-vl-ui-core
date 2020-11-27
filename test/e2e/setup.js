fa416c0ad6f368aacb1391c002864a882807fcb7
Tom Coemans <tom.coemans@gmail.com>
2020-11-27 09:57:00 +0100

    change browserstack session name

--- test/e2e/setup.js
+++ test/e2e/setup.js
@@ -1,6 +1,7 @@
 const {By, Key, Builder} = require('selenium-webdriver');
 const browserstack = require('browserstack-local');
 const config = require('./config');
+const packagejson = require('./package.json');
 
 const chai = require('chai');
 const chaiAsPromised = require('chai-as-promised');
@@ -13,7 +14,7 @@ const capabilities = {
   'os_version': config.osVersion,
   'browserName': config.browserName,
   'browser_version': config.browserVersion,
-  'name': 'Webcomponenten',
+  'name': packagejson.name,
   'build': 'Milieuinfo',
   'browserstack.user': `${process.env.browserstack_username}`,
   'browserstack.key': `${process.env.browserstack_password}`,
