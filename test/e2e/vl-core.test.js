95bdca8e9639fdb8fa1dc0575151b1cbcec2f4dc
Tom Coemans <tom.coemans@gmail.com>
2020-11-27 09:35:05 +0100

    fix e2e test

--- test/e2e/vl-core.test.js
+++ test/e2e/vl-core.test.js
@@ -12,9 +12,9 @@ describe('vl-core', async () => {
   });
 
   it('een element wordt pas visible als het defined wordt', async () => {
-    await vlCorePage.load();
     const slowElement = await vlCorePage.getSlowElement();
-    await assert.eventually.isFalse(slowElement.isDisplayed());
     await driver.wait(async () => await slowElement.isDisplayed(), 3000);
+    await vlCorePage.load();
+    await assert.eventually.isFalse(slowElement.isDisplayed());
   });
 });
