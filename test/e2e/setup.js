e3aee32f8ab08b822f1e40960f03bed008b42a74
Tom Coemans <tom.coemans@gmail.com>
2020-12-02 09:20:58 +0100

    enable network logs

--- test/e2e/setup.js
+++ test/e2e/setup.js
@@ -28,6 +28,7 @@ const capabilities = {
   'browserstack.local': true,
   'browserstack.localIdentifier': identifier,
   'browserstack.selenium_version': '4.0.0-alpha-6',
+  'browserstack.networkLogs': true,
 };
 
 const startConfig = {
