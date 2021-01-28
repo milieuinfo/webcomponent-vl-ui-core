b320b777d97b401b9f02f119145b70370dd5a256
Tom Coemans <tom.coemans@gmail.com>
2021-01-28 16:02:27 +0100

    fixed import

--- src/index.js
+++ src/index.js
@@ -1,7 +1,16 @@
 import {vlElement, nativeVlElement, define, awaitScript, awaitUntil} from '../dist/vl-core.src.js';
-import VlText from '../dist/vl-text.src.js';
+import {VlText} from '../dist/vl-text.src.js';
 
 const VlElement = vlElement;
 const NativeVlElement = nativeVlElement;
 
-export {vlElement, VlElement, nativeVlElement, NativeVlElement, define, awaitScript, awaitUntil, VlText};
+export {
+    vlElement,
+    VlElement,
+    nativeVlElement,
+    NativeVlElement,
+    define,
+    awaitScript,
+    awaitUntil,
+    VlText
+};
