5e22c72b54390e148ee9f42b091f4b3930649e71
Tom Coemans <tom.coemans@gmail.com>
2021-01-29 09:56:43 +0100

    update dependencies

--- src/index.js
+++ src/index.js
@@ -1,16 +1,1 @@
-import {vlElement, nativeVlElement, define, awaitScript, awaitUntil} from '../dist/vl-core.src.js';
-import {VlText} from '../dist/vl-text.src.js';
-
-const VlElement = vlElement;
-const NativeVlElement = nativeVlElement;
-
-export {
-    vlElement,
-    VlElement,
-    nativeVlElement,
-    NativeVlElement,
-    define,
-    awaitScript,
-    awaitUntil,
-    VlText
-};
+export * from '../dist/vl-core-all.src.js';
