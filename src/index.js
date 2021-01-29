e238557a37904fe5263b8c57410e879619462cc0
coemans <tom.coemans@vlaanderen.be>
2021-01-29 14:05:27 +0100

    CommonJS imports vervangen door ES varianten (#128)

--- src/index.js
+++ src/index.js
@@ -1,8 +1,1 @@
-const {vlElement, nativeVlElement, define, awaitScript, awaitUntil} = require('../dist/vl-core.src.js');
-const VlText = require('../dist/vl-text.src.js');
-const VlElement = vlElement;
-const NativeVlElement = nativeVlElement;
-
-module.exports = {
-  vlElement, VlElement, nativeVlElement, NativeVlElement, define, awaitScript, awaitUntil, VlText,
-};
+export * from '../dist/vl-core-all.src.js';
