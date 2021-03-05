dd24c406e46554aa7af2e247c9e64eda7dba37dc
coemans <tom.coemans@zeuzios.be>
2021-03-05 09:45:36 +0100

    Page object uitbreiding om op e2e test niveau WCAG problemen te detecteren (#135)

--- test/e2e/pages/page.js
+++ test/e2e/pages/page.js
@@ -1,3 +1,5 @@
+const axeBuilder = require('axe-webdriverjs');
+
 class Page {
   constructor(driver) {
     this.driver = driver;
@@ -6,6 +8,17 @@ class Page {
   async load(url) {
     await this.driver.get(`${url}?no-header=true&no-footer=true`);
     await this.driver.manage().window().maximize();
+  }
+
+  async hasWcagIssues() {
+    const report = await axeBuilder(this.driver).analyze();
+    const violations = report.violations;
+    if (violations && violations.length > 0) {
+      violations.map((violation, index) => console.error(`WCAG issue ${++index}: ${violation.description}`));
+      return true;
+    } else {
+      return false;
+    }
   }
 }
 
